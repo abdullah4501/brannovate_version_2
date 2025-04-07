c2afb4d5f53a95678f200f299eae3336000a6a16
Abdullah Ahmed <abdullah.ah452001@gmail.com>
2025-04-07 18:37:25 +0500

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import ScrollToTop from './components/common/ScrollToTop';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <ScrollToTop>
         <Home />
-      </ScrollToTop>
     </Router>
   );
 }
